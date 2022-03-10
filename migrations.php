6852b8eec9c771b2c88ef4d9766f2aed606cc809
AlexFedyshyn <alex.fedyshyn.v@gmail.com>
2022-03-10 11:57:23 +0200

    migrations

--- migrations.php
+++ migrations.php
@@ -1,2 +1,28 @@
 <?php
 
+use app\core\user\controllers\AuthController;
+use app\core\user\controllers\SiteController;
+use app\core\base\controllers\Application;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+$app = new Application(__DIR__, $config);
+
+$app->db->applyMigrations();
+
+
+
+
+
+
