fea84cf9a2e6d32bc9425cc179db643417a40c13
AlexFedyshyn <alex.fedyshyn.v@gmail.com>
2022-03-15 11:43:03 +0200

    registration form user friendli text

--- core/base/models/User.php
+++ core/base/models/User.php
@@ -48,5 +48,16 @@ class User extends DbModel
         return ['firstname', 'lastname', 'email', 'password', 'status'];
     }
 
+    public function labels(): array
+    {
+        return [
+            'firstname' => 'First name',
+            'lastname' => 'Last name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'passwordConfirm' => 'Confirm password',
+        ];
+    }
+
 
 }
